d037c9944847a274ef3aa4984046a68bdaed7980
Lee <emg2634@gmail.com>
2024-04-30 15:01:33 +0900

    tip-base범위 고침

--- main_potential_profile_tip_1D.py
+++ main_potential_profile_tip_1D.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # 함수 정의
 def z(x, y, B, λ):
-    return B * (np.cos(np.pi * 2 * x / λ) * np.cos(np.pi * 2 * y / λ))
+    return B * (np.cos(np.pi * 2 * x / λ) + np.cos(np.pi * 2 * y / λ))
 
 
 # 초기 설정
